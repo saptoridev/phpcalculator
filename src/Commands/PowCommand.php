796a9bd9400f32f02508455c6c470d76ed9c24fc
saptoridev <saptoridev@gmail.com>
2020-08-03 06:58:41 +0700

    add pow command

---
+++ src/Commands/PowCommand.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Jakmall\Recruitment\Calculator\Commands;
+
+use Illuminate\Console\Command;
+
+class PowCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    public function __construct()
+    {
+        $commandVerb = $this->getCommandVerb();
+
+        $this->signature = sprintf(
+            '%s {base : The base number} {exp : The exponent number}',
+            $commandVerb
+        );
+        $this->description = sprintf('Exponent the given number');
+
+        parent::__construct();
+    }
+
+    protected function getCommandVerb(): string
+    {
+        return 'pow';
+    }
+   
+    public function handle(): void
+    {
+        $base = $this->getBaseInput();
+        $exp = $this->getExpInput();
+        $description = $this->generateCalculationDescription($base,$exp);
+        $result = $this->calculate($base,$exp);
+
+        $this->comment(sprintf('%s = %s', $description, $result));
+    }
+
+    protected function getBaseInput()
+    {
+        return $this->argument('base');
+    }
+
+    protected function getExpInput()
+    {
+        return $this->argument('exp');
+    }
+
+
+    protected function generateCalculationDescription(int $base,int $exp): string
+    {
+        $operator = $this->getOperator();
+        return sprintf("%s %s %s",$base,$operator,$exp);
+    }
+
+    protected function getOperator(): string
+    {
+        return '^';
+    }
+
+  
+    /**
+     * @param int|float $base
+     * @param int|float $exp
+     *
+     * @return int|float
+     */
+    protected function calculate($base, $exp)
+    {
+        return pow($base,$exp);
+    }
+}
